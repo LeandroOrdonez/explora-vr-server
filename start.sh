f05fd0fc8258be4ea378c3737d522c2c24ca71fe
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2021-04-17 19:50:45 +0200

    Update traffic control config

--- start.sh
+++ start.sh
@@ -4,7 +4,13 @@ if [ "$ENABLE_TC" == "true" ]
 then
     tc qdisc add dev eth0 root handle 1: htb default 12
     tc class add dev eth0 parent 1:1 classid 1:12 htb rate ${BANDWIDTH}
-    tc qdisc add dev eth0 parent 1:12 netem delay ${LATENCY} ${JITTER} distribution normal
+    if [ "$LATENCY" -gt 0 ]; then
+        if [ "$JITTER" -gt 0 ]; then
+            tc qdisc add dev eth0 parent 1:12 netem delay ${LATENCY}ms ${JITTER}ms distribution normal
+        else
+            tc qdisc add dev eth0 parent 1:12 netem delay ${LATENCY}ms
+        fi
+    fi        
 fi
 
 service nginx start
